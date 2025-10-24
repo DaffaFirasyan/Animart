6eab84c185de677bffd7772a6a83219d9fe36e6a
DaffaFirasyan <firasyan.daffa123@gmail.com>
2025-10-24 14:35:47 +0700

    Redirect halaman awal ke login

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\LaporanController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // Grup route yang HANYA bisa diakses setelah login
